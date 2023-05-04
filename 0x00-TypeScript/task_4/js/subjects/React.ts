51123fd5be36036c6491c2e3ecb646c7b3e208e2
Khaoula <daouaykhawla782@gmail.com>
2023-05-04 23:08:50 +0100

    all

---
+++ 0x00-TypeScript/task_4/js/subjects/React.ts
@@ -0,0 +1,18 @@
+namespace Subjects {
+    export interface TeacherInterface {
+      experienceTeachingReact?: number;
+    }
+    export class React extends Subject {
+      getRequirements(): string {
+        return "Here is the list of requirements for React";
+      }
+      getAvailableTeacher(): string {
+        if (this.teacher.experienceTeachingReact === undefined) {
+          return "No available teacher";
+        } else {
+        return `Available Teacher: ${this.teacher.firstName}`;
+        }
+      }
+    }
+  }
+  
