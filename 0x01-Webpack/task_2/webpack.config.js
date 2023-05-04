24c9535f04ba2e4da45517fd8e5d590153d2201e
Khaoula <daouaykhawla782@gmail.com>
2023-05-05 00:01:53 +0100

    task2

--- 0x01-Webpack/task_2/webpack.config.js
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -1,27 +1,34 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './js/dashboard_main.js',
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
-    publicPath: '/public'
+  mode: "production",
+  entry: "./js/dashboard_main.js",
+  performance: {
+    maxAssetSize: 1000000,
   },
-  mode: 'production',
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(jpg|png|gif|svg)$/,
-        loader: 'image-webpack-loader',
-        enforce: 'pre'
-      }
-    ]
-  }
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
